9e618764655943ed54d6ae0d49275d59f272cbb5
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-04-27 11:38:44 -0700

    add in gpt4 refactored code, tests

---
+++ README.md
@@ -0,0 +1,19 @@
+# TS-HNSW
+
+[![npm Version](https://img.shields.io/npm/v/ts-hnsw.svg)](https://www.npmjs.com/package/ts-hnsw) [![License](https://img.shields.io/npm/l/ts-hnsw.svg)](https://www.npmjs.com/package/ts-hnsw)
+
+Pure typescript implementation of HNSW
+
+# Introduction
+
+# Getting Started
+
+```
+yarn add ts-hnsw
+```
+
+or
+
+```
+npm i --save ts-hnsw
+```
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    dir: "test",
+    watch: false,
+    testTimeout: 5000,
+    passWithNoTests: true,
+    reporters: ["verbose"],
+    coverage: { reporter: ["lcov", "html", "text"] },
+    deps: {
+      interopDefault: true,
+      registerNodeLoader: true,
+    },
+  },
+});
